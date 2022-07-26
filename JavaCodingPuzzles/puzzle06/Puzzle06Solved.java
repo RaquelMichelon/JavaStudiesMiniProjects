dde5a7574a2133bb5d7f57e1b48d85b6960ff05a
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-07-26 08:19:11 +0100

    Create Puzzle06Solved.java

---
+++ JavaCodingPuzzles/puzzle06/Puzzle06Solved.java
@@ -0,0 +1,14 @@
+package puzzle06;
+
+public class Puzzle06Solved {
+	
+	public static void main(String[] args) {
+		System.out.println(fun());
+	}
+	
+	static int fun() {
+		int x = 0; //or final int x = 0; -> if I need a constant
+		return ++x;
+	}
+
+}
