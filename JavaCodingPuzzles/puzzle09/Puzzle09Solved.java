86ebf8402c036d04ecbc235f42aba7e4e8a765b2
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-07-27 08:44:22 +0100

    Create Puzzle09Solved.java

---
+++ JavaCodingPuzzles/puzzle09/Puzzle09Solved.java
@@ -0,0 +1,13 @@
+package puzzle09;
+
+public class Puzzle09Solved {
+	
+	public static void main(String[] args) { 
+		System.out.println(fun());
+	}
+	
+	public static int fun() { 
+		return 20;
+	}
+
+}
