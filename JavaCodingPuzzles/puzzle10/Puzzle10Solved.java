f68cb2e2b2bac1d241d8a090094a9d1e182d8f42
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-07-27 08:55:05 +0100

    Create Puzzle10Solved.java

---
+++ JavaCodingPuzzles/puzzle10/Puzzle10Solved.java
@@ -0,0 +1,12 @@
+package puzzle10;
+
+public class Puzzle10Solved {
+	
+	static int i;
+	
+	public static void main(String[] args) { 
+		int i = 10;
+		System.out.println(i); //local variable will take the priority here, but there will be a compilation error here.
+	}
+
+}
