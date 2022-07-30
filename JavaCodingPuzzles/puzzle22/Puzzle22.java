1e2ffe4c1e3df9dd6f25c5376031e90a91fd822f
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-07-30 20:31:39 +0100

    Create Puzzle22.java

---
+++ JavaCodingPuzzles/puzzle22/Puzzle22.java
@@ -0,0 +1,22 @@
+package package22;
+/*
+ * Will this code compile? If yes, what will be the output.
+ */
+
+public class Puzzle22 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int count = 0;
+		
+		for(i = 0; i <= 10; i++) {
+			count++;
+		}
+		
+		System.out.print(count); //11
+		System.out.print(i); //11
+	}
+
+}
+
+
