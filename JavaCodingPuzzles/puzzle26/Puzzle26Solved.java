a0726dab07852e1d454e1453ee554bc7e6e04164
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-07-31 15:55:28 +0100

    Create Puzzle26Solved.java

---
+++ JavaCodingPuzzles/puzzle26/Puzzle26Solved.java
@@ -0,0 +1,22 @@
+package puzzle26;
+
+/*
+ * Will this code compile? If yes, what will be the output.
+ */
+
+
+public class Puzzle26Solved { 
+
+	public static void main(String[] args) {
+		int i = 20;
+		double d = i; 
+		
+		int j = (int)d;  //20
+		double d1 = 10; 
+		
+		System.out.print(j / d1); //2.0
+
+
+	}
+
+}
