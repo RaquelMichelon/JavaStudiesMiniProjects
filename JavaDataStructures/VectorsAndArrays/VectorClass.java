ba1cb061dce47ee385feb7f84049f84ac0a0dc6b
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-02 09:59:05 +0100

    Create VectorClass.java

---
+++ JavaDataStructures/VectorsAndArrays/VectorClass.java
@@ -0,0 +1,26 @@
+package datastructures;
+
+/*
+ * Vectors are used to store data from the same type.
+ */
+
+public class VectorClass {
+
+	public static void main(String[] args) {
+		// E.g. store temperatures for each day in one year inside an array
+		double[] temperatures = new double[365];
+		temperatures[0] = 39.0;
+		temperatures[1] = 38.0;
+		temperatures[2] = 35.8;
+		
+		//to access one position in an array
+		//temperatures[1]
+		
+		//get the array length using the length property
+		//temperatures.length
+		
+		//iteration with for simple or advanced
+
+	}
+
+}
