764c3e93c7988575cd12cdf7f760102b0b2360a9
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-05 09:32:15 +0100

    Create ReadDataThread.java

---
+++ Threads/ExampleSource/src/ReadDataThread.java
@@ -0,0 +1,13 @@
+public class ReadDataThread implements Runnable {
+    private Data data;
+
+    public ReadDataThread(Data data) {
+        super();
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        data.read();
+    }
+}
