adfa4cf977bb46c0d89ca94aa561a9065c8c3c55
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-05 09:32:45 +0100

    Create SyncDataThread.java

---
+++ Threads/ExampleSource/src/SyncDataThread.java
@@ -0,0 +1,13 @@
+public class SyncDataThread implements  Runnable {
+    private Data data;
+
+    public SyncDataThread(Data data) {
+        super();
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        data.sync();
+    }
+}
