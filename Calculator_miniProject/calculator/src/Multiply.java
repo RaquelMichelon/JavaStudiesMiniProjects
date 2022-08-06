130dc55c2988def80bcc7a0d5e0281c26f08ed9d
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-06 18:40:45 +0100

    Create Multiply.java

---
+++ Calculator_miniProject/calculator/src/Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply implements Operate {
+    @Override
+    public Double getResult(Double... numbers) {
+        Double result = 1.0;
+
+        for (Double num : numbers) {
+            result *= num;
+        }
+        return result;
+    }
+}
