852ed3f0b8a18202c5d22b75a4e5f64e8d781bae
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-10 08:09:45 +0100

    Create Employee.java

---
+++ JavaAPIStreams/HowToCreateAStream/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+
+    private int id;
+    private String name;
+    private double income;
+
+    public Employee(int id, String name, double income) {
+        this.id = id;
+        this.name = name;
+        this.income = income;
+    }
+}
