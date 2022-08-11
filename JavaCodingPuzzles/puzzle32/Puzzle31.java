9d030383fbd179289634b8519183925ab8c51d65
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-11 09:23:17 +0100

    Create Puzzle31.java

---
+++ JavaCodingPuzzles/puzzle32/Puzzle31.java
@@ -0,0 +1,27 @@
+package puzzle31;
+
+/*
+ * Will this code compile. If yes, what will be the output.
+ * 
+ * This code will print 30, then print END!!!, then return true
+ */
+public class Puzzle31 {
+
+	public static void main(String[] args) {
+		method1();
+
+	}
+	
+	public static boolean method1() {
+		int[] a = {10, 20, 30};
+		
+		try {
+			System.out.println(a[2]);
+			return true; // first execute the finally block, then return the fuction
+		} finally {
+			//finally block is always executed regardless of the exception we are getting or not
+			System.out.println("END!!!");
+		}
+	}
+
+}
