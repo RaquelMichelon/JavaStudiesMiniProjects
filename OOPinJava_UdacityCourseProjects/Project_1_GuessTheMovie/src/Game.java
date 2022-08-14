ae9a16ea2724c92a153e6a857bb17f65d943c122
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-14 18:59:00 +0100

    Step 2 randomly pick one movie and display it

--- OOPinJava_UdacityCourseProjects/Project_1_GuessTheMovie/src/Game.java
+++ OOPinJava_UdacityCourseProjects/Project_1_GuessTheMovie/src/Game.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
@@ -9,11 +10,17 @@ public class Game {
     private String movieName;
 
     //set the name of the movie
+    public void getRandomMovie(List<String> allMovies) {
+        allMovies = displayMovies();
+        Random chooseMovie = new Random();
+        String selectedMovie = allMovies.get(chooseMovie.nextInt(allMovies.size()));
+        System.out.printf("Picked movie is: %s", selectedMovie);
+    }
 
     //handle a single guess
 
     //display the hidden movie title
-    public void displayMovies() {
+    public List<String> displayMovies() {
         File file = new File("movies.txt");
         List<String> moviesList = new ArrayList<String>();
         Scanner scanner;
@@ -26,7 +33,8 @@ public class Game {
             moviesList.add(scanner.nextLine());
         }
         System.out.println(moviesList);
+
+        return moviesList;
     }
 
 }
-
