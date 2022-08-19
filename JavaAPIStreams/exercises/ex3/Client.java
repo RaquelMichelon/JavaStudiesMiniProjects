60aa5ca88c1e2e831fc52ce28dd4a3b913d5f39e
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-19 10:57:19 +0100

    Create Client.java

---
+++ JavaAPIStreams/exercises/ex3/Client.java
@@ -0,0 +1,20 @@
+public class Client {
+  
+  private String name;
+  private int points;
+  
+  
+  public Client(String name, int points) {
+    this.name = name;
+    this.points = points;
+  }
+  
+  public String getName() {
+    return this.name;
+  }
+  
+  public int getPoints() {
+    return this.points;
+  }
+  
+}
