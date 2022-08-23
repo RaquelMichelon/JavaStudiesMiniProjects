02568705d761bd86b9a60ccec9bda9924b306b2a
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-23 20:21:41 +0100

    Create Main.java

---
+++ Java_Generate_HTML/Ex_01/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    //1. ASK USER FOR A FILE NAME
+    
+    //2. CHECK IF THE FILE EXIST
+    
+    //3. DETERMINE IF A FILE IS EMPTY
+    
+    //4. RENAME .TXT AS .HTML
+    
+    //5. READ EACH LINE AND INSERT NECESSARY TAGS
+  }
+}
