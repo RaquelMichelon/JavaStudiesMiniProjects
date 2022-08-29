b2d7ce1b577cc1456fd04e309503eb9422f43fe0
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-29 18:12:58 +0100

    Create Main.java

---
+++ SimpleJavaPrograms/CommandLineArg/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //command line argument in java
+        for (String string: args) {
+            System.out.println(string);
+        }
+    }
+}
