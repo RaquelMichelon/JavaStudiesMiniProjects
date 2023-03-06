4bf3236b8c0ce57c78d6f979edaa7090df5f6563
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2023-03-06 07:46:07 +0000

    Update App.java

--- JavaDataStructures/Stacks/Example02/App.java
+++ JavaDataStructures/Stacks/Example02/App.java
@@ -25,6 +25,20 @@ public class App {
         System.out.println(carStack);
 
         System.out.println("Stack is empty? " + carStack.empty());
+        System.out.println("Does the stack contains 'Fiat'? " + carStack.search(new Car("Fiat")));
+
+        //Returns the position the element searched is from the top; The top element returns 1, The second element from
+        //the top returns 2 and so on. If the element isn't found, it returns -1.
+        Stack<Integer> numberStack = new Stack<>();
+        numberStack.push(1);
+        numberStack.push(2);
+        numberStack.push(3);
+        numberStack.push(4);
+        numberStack.push(5);
+        System.out.println(numberStack);
+        System.out.println("Does the stack contains '5'? " + numberStack.search(5)); //1 = because it is the first element on the stack
+        System.out.println("Does the stack contains '4'? " + numberStack.search(4)); //2 = because it is the second element on the stack
+        System.out.println("Does the stack contains '6'? " + numberStack.search(6)); //-1 = because it is not on the stack
 
     }
 }
